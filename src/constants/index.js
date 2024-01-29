cfb3930291a0b21f7c6b8956473a7c2b2ef8d337
Dilshad <hmydilshadkt@gmail.com>
2024-01-29 20:54:06 +0530

    fix:profile section

--- src/constants/index.js
+++ src/constants/index.js
@@ -179,12 +179,12 @@ export const socialLinks = [
   {
     name: "GitHub",
     iconUrl: github,
-    link: "https://github.com/YourGitHubUsername",
+    link: "https://github.com/dilshadkt",
   },
   {
     name: "LinkedIn",
     iconUrl: linkedin,
-    link: "https://www.linkedin.com/in/YourLinkedInUsername",
+    link: "https://www.linkedin.com/in/dilshad-kt-1389b3255/",
   },
 ];
 
